e6581394479523312bd8e15f2ac477a9975edb23
LENOVO <sworupdhakal88@gmail.com>
2024-08-08 21:00:07 +0545

    j

--- src/billing.java
+++ src/billing.java
@@ -4,14 +4,14 @@ public class billing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean start = true;
-        while(start){
+        int n;
+        while (start) {
             System.out.println("Do you want to start or quit?");
             String stat = sc.nextLine();
 
-            if(stat.equalsIgnoreCase("quit")){
+            if (stat.equalsIgnoreCase("quit")) {
                 start = false;
-            }
-            else{
+            } else {
                 ArrayList<Integer> itemcost = new ArrayList<Integer>();
 
                 System.out.println("Enter your ID");
@@ -24,16 +24,83 @@ public class billing {
                 System.out.println("Enter the number of items purchased");
                 int item = sc.nextInt();
 
-                for(int i = 0; i<item; i++){
+                for (int i = 0; i < item; i++) {
                     System.out.println("Enter the price of item");
                     int cost = sc.nextInt();
                     itemcost.add(cost);
                 }
-            System.out.println
+                int totalcost = 0;
+                for (int cost : itemcost) {
+                    totalcost += cost;
+                }
+                sc.nextLine();
+                System.out.println("Do you want to be charged VAT?(yes/no)");
+                String VAT = sc.nextLine();
 
-            }
 
+                if (VAT.equalsIgnoreCase("yes")) {
+                    totalcost += totalcost * 0.13;
+                    System.out.println("VAT charged.");
+                }
+
+                System.out.println("Do you have Discount to apply?(yes/no)");
+                String Dis = sc.nextLine();
+
+                if (Dis.equalsIgnoreCase("yes")) {
+                    System.out.println("Enter Discount Percentage");
+                    int discount = sc.nextInt();
+                    totalcost -= totalcost * discount / 100;
+                    System.out.println("Discount has been applied");
+                }
+                System.out.println("The total cost is: " + totalcost);
+                System.out.println("Enter Amount of cash you want to pay");
+                int money = sc.nextInt();
+
+                int change = money - totalcost;
+
+                if (change >= 1000);
+                n = change / 1000;
+                System.out.println("Amount of 1000: " +n);
+                change = change - n * 1000;
+
+                if(change >= 500);
+                n = change / 500;
+                System.out.println("Amount of 500: " +n);
+                change = change - n * 500;
+
+                if(change >= 100);
+                n = change / 100;
+                System.out.println("Amount of 100: "+n);
+                change = change - n * 100;
+
+                if(change >= 50);
+                n = change / 50;
+                System.out.println("Amount of 50: "+n);
+                change = change - n * 50;
+
+                if(change >= 20);
+                n = change / 20;
+                System.out.println("Amount of 20: "+n);
+                change = change - n *20;
+
+                if(change >= 10);
+                n = change / 10;
+                System.out.println("Amount of 10: "+n);
+                change = change - n * 10;
+
+                if(change >= 5);
+                n = change / 5;
+                System.out.println("Amount of 5: "+n);
+                change = change - n * 5;
+
+                if(change >= 1);
+                n = change / 1;
+                System.out.println("Amount of 1: "+n);
+
+                System.out.println("Thanks for choosing us! Have a great day ahead!!");
             }
         }
+    sc.close();
     }
+}
 
